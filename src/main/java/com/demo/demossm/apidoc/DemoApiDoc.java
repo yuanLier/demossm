93aee7a1e24bffc6138c6716e74bc16f9031a60a
yuanyiwen <yuan15826770919@126.com>
2018-08-27 13:49:18 +0800

    最后加上假·接口文档 完美

--- src/main/java/com/demo/demossm/apidoc/DemoApiDoc.java
+++ src/main/java/com/demo/demossm/apidoc/DemoApiDoc.java
@@ -1,4 +1,22 @@
 package com.demo.demossm.apidoc;
 
+import com.demo.demossm.model.Demo;
+
 public class DemoApiDoc {
+
+    /**
+     *   @api {GET} demo 查找单个成员信息
+     *   @apiDescription 根据id找成员信息
+     *   @apiGroup DEMO-QUERY
+     *   @apiParam  {String} id 成员id
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "id": "001",
+     *         "name": "小明"
+     *       }
+     */
+    public Demo selectOne(String id){
+        return new Demo();
+    }
 }
